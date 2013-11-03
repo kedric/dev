2433a2b0b9578ef1bb0fb87df716020de70c4bef
kedric <kedric@me.com>
2013-11-03 10:03:06 +0000

    j02/ex04 V2

--- j02/ex04/ft_print_comb.c
+++ j02/ex04/ft_print_comb.c
@@ -1,31 +1,26 @@
-void ft_putnbr(int i);
-void ft_putchar(char c);
-
-void ft_print_comb(void)
+void	ft_print_comb(void)
 {
   int i;
 
   i = 000;
-  while(i < 790)
+
+  while(i <= 789)
     {
-      if(i/100 < i / 10 % 10 && i / 10 % 10 < i % 10 && i < 789)
+      if(i / 100 < i / 10 % 10 && i / 10 % 10 < i % 10)
 	{
-	ft_putnbr(i/100);
-	ft_putnbr(i/10%10);
-	ft_putnbr(i%10);
-	ft_putchar(',');
-	ft_putchar(' ');
-	}
-     else if(i == 789)
-	{
-	  ft_putnbr(i);
+	  ft_putchar('0' + i / 100);
+	  ft_putchar('0' + i / 10 % 10);
+	  ft_putchar('0' + i % 10);
+	  if(i < 789)
+	    {
+	      ft_putchar(' ');
+	      ft_putchar(';');
+	    }
 	}
       i++;
     }
 }
-
-int main()
+int	main()
 {
   ft_print_comb();
-  return(0);
 }
