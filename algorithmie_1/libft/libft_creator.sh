22a98addde07003ccd400bd4aada82adffdbc719
Jean-Luc MANCERON <jmancero@e3r11p8.42.fr>
2013-11-19 20:01:41 +0100

    rm

--- algorithmie_1/libft/libft_creator.sh
+++
@@ -1,7 +0,0 @@
-gcc -c ft_putchar.c
-gcc -c ft_putstr.c
-gcc -c ft_strcmp.c
-gcc -c ft_strlen.c
-gcc -c ft_swap.c
-ar rc libft.a ft_putchar.o ft_putstr.o ft_strcmp.o ft_strlen.c ft_swap.o
-ranlib libft.a
