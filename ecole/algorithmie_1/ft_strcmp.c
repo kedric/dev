872ac6f96b49208ea01c7d0dc1c0e0192edea1d4
Jean-Luc MANCERON <jmancero@e3r11p8.42.fr>
2013-11-20 12:16:09 +0100

    mise au norme

--- ecole/algorithmie_1/ft_strcmp.c
+++ ecole/algorithmie_1/ft_strcmp.c
@@ -1,23 +1,34 @@
-int	ft_strcmp(char *s1, char *s2)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmancero <jmancero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/20 12:08:04 by jmancero          #+#    #+#             */
+/*   Updated: 2013/11/20 12:09:13 by jmancero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int		ft_strcmp(char *s1, char *s2)
 {
-  int i;
-  int value_s1;
-  int value_s2;
+	int	i;
+	int	value_s1;
+	int	value_s2;
 
-  i = 0;
-  value_s1 = 0;
-  value_s2 = 0;
+	i = 0;
+	value_s1 = 0;
+	value_s2 = 0;
 
-  while(s1[i] != '\0')
-    {
-      value_s1 = value_s1 + s1[i];
-      i++;
-    }
-  i = 0;
-  while(s2[i] != '\0')
-    {
-      value_s2 = value_s2 + s2[i];
-      i++;
-    }
-  return value_s1 - value_s2;
+	while(s1[i] != '\0')
+	{
+		value_s1 = value_s1 + s1[i];
+		i++;
+	}
+	i = 0;
+	while(s2[i] != '\0')
+	{
+		value_s2 = value_s2 + s2[i];
+		i++;
+	}
+	return value_s1 - value_s2;
 }
--- ecole/algorithmie_1/ft_strstr.c
+++ ecole/algorithmie_1/ft_strstr.c
@@ -1,45 +1,57 @@
-int	ft_verif(char *str, char *to_find)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmancero <jmancero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/20 12:13:51 by jmancero          #+#    #+#             */
+/*   Updated: 2013/11/20 12:15:41 by jmancero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int		ft_verif(char *str, char *to_find)
 {
-  int i = 0;
+	int i = 0;
 
-  while(str[i] == to_find[i])
-    {
-      i++;
-    }
-  if (to_find[i] == '\0')
-    {
-      return 1;
-    }
-  else 
-    {
-      return 0;
-    }
+	while(str[i] == to_find[i])
+	{
+		i++;
+	}
+	if (to_find[i] == '\0')
+	{
+		return 1;
+	}
+	else 
+	{
+		return 0;
+	}
 }
+
 char	*ft_strstr(char *str, char *to_find)
 {
-  int i;
-  int match;
-  
-  i = 0;
-  match = 0;
+	int	i;
+	int	match;
 
-  while(str[i] != '\0' || match != 1)
-    {
-      if (str[i] == to_find[0])
+	i = 0;
+	match = 0;
+
+	while(str[i] != '\0' || match != 1)
 	{
-	 match = ft_verif(str + i, to_find);
+		if (str[i] == to_find[0])
+		{
+			match = ft_verif(str + i, to_find);
+		}
+		if(match != 1)
+		{
+			i++;
+		}
 	}
-      if(match != 1)
+	if (match == 1)
 	{
-	  i++;
+		return str + i;
 	}
-    }
-  if (match == 1)
-    {
-      return str + i;
-    }
-  else 
-    {
-      return 0;
-    }
+	else 
+	{
+		return 0;
+	}
 }
