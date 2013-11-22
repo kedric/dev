b1143fa464eb445f31d05d9ce9ba24f2d6ca8504
Jean-Luc MANCERON <jmancero@e3r10p10.42.fr>
2013-11-22 20:00:04 +0100

    cleans

--- ecole/algorithmie_1/libft.h
+++ ecole/algorithmie_1/libft.h
@@ -1,3 +1,18 @@
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	ft_putchar(char c);
+void	ft_putnbr(int n);
+void	ft_putstr(char const *s);
+int		ft_strcmp(const char *s1,const char *s2);
+char	*ft_strcpy(char *s1, const char *s2);
+int		ft_strlen(char *s);
+char	*ft_strncpy(char *s1, char *s2, size_t n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+void    *ft_memccpy(void *s1, const void *s2, int c,size_t n);
