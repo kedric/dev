43f99ac72a39a9cc8fba8c49f0e04699ef3a323d
kedric <kedric@me.com>
2013-11-23 13:55:35 +0100

    lol

--- ecole/algorithmie_1/libft/fait/ft_memcmp.c
+++ ecole/algorithmie_1/libft/fait/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jmancero <jmancero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/23 10:26:56 by jmancero          #+#    #+#             */
-/*   Updated: 2013/11/23 13:53:07 by jmancero         ###   ########.fr       */
+/*   Updated: 2013/11/23 13:54:50 by jmancero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <string.h>
@@ -19,7 +19,7 @@ int ft_memcmp(const void *s1, const void *s2, size_t n)
 	if(s1 && s2)
 	{
 		while(((*((char *)s1 +i) != '\0') && (n > i)) 
-			  || ((*((char *)s2 + i) != '\0') && (n > i))
+			  || ((*((char *)s2 + i) != '\0') && (n > i)))
 		{
 			if (*((char *)s1 + i) > *((char *)s2 + i))
 				return (1);
